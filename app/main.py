385f4d6961ae54266340228a994c47a1f64023d5
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-08-18 17:48:01 +0400

    Подключение к БД

--- app/main.py
+++ app/main.py
@@ -1,9 +1,30 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from datetime import date
+from typing import Optional
+from pydantic import BaseModel
 
 
 app = FastAPI()
 
 
 @app.get("/hotels")
-def get_hotels():
-    return "Отель Бридж Резорт 5 звезд"
+def get_hotels(
+        location: str,
+        date_from: date,
+        date_to: date,
+        has_spa: Optional[bool] = None,
+        stars: Optional[int] = Query(None, ge=1, le=5),
+):
+    print(date_to, date_from, has_spa, stars)
+    return date_from, date_to
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post("/booking")
+def add_booking(booking: SBooking):
+    pass
