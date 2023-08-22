6c000a2ed24556f5ae65e0f42375cd2163b928d6
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-08-22 19:12:56 +0400

    Добавлен роутер для регистрации пользователя

--- app/main.py
+++ app/main.py
@@ -1,34 +1,11 @@
-from fastapi import FastAPI, Query
-from datetime import date
-from typing import Optional
-from pydantic import BaseModel
+from fastapi import FastAPI
 
 from app.bookings.router import router as router_bookings
+from app.users.router import router as router_users
 
 
 app = FastAPI()
 
+app.include_router(router_users)
 app.include_router(router_bookings)
 
-
-@app.get("/hotels")
-def get_hotels(
-        location: str,
-        date_from: date,
-        date_to: date,
-        has_spa: Optional[bool] = None,
-        stars: Optional[int] = Query(None, ge=1, le=5),
-):
-    print(date_to, date_from, has_spa, stars)
-    return date_from, date_to
-
-
-class SBooking(BaseModel):
-    room_id: int
-    date_from: date
-    date_to: date
-
-
-@app.post("/booking")
-def add_booking(booking: SBooking):
-    pass
