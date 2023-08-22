5bab1c1dac58f2d1e784d442ab7ea4853fa73ea7
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-08-22 17:15:11 +0400

    Добавление schemas

--- app/bookings/router.py
+++ app/bookings/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from app.bookings.dao import BookingDAO
-
+from app.bookings.schemas import SBooking
 
 router = APIRouter(
     prefix="/bookings",
@@ -10,8 +10,8 @@ router = APIRouter(
 
 
 @router.get("")
-async def get_bookings():
-    return await BookingDAO.find_by_id(1)
+async def get_bookings() -> list[SBooking]:
+    return await BookingDAO.find_all()
 
 
 
