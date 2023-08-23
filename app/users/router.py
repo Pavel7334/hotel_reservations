56b08dbc0532d71bd98134c3dd1d6277cbb55689
Игонин Павел <88630513+Pavel7334@users.noreply.github.com>
2023-08-23 18:35:11 +0400

    Добавлен ротер на logout, изменён роутер get_bookings, dependencies

--- app/users/router.py
+++ app/users/router.py
@@ -1,8 +1,10 @@
-from fastapi import APIRouter, Response
+from fastapi import APIRouter, Response, Depends
 
 from app.exceptions import UserAlreadyExistsException, IncorrectEmailOrPasswordException
 from app.users.auth import get_password_hash, authenticate_user, create_access_token
 from app.users.dao import UsersDAO
+from app.users.dependencies import get_current_user
+from app.users.models import Users
 from app.users.schemas import SUserAuth
 
 router = APIRouter(
@@ -28,3 +30,19 @@ async def login_user(response: Response, user_data: SUserAuth):
     access_token = create_access_token({"sub": str(user.id)})
     response.set_cookie("booking_access_token", access_token, httponly=True)
     return {"access_token": access_token}
+
+
+@router.post("/logout")
+async def logout_user(response: Response):
+    response.delete_cookie("booking_access_token")
+    return f"Пользователь вышел из системы"
+
+
+@router.get("/me")
+async def read_users_me(current_user: Users = Depends(get_current_user)):
+    return current_user
+#
+#
+# @router.get("/all")
+# async def read_users_all(current_user: Users = Depends(get_current_admin_user)):
+#     return await UsersDAO.find_all()
